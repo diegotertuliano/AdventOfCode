39035d2b0f0e93b94230f67200c03db551dd4f82
Diego Tertuliano <diego.tertuliano@vtex.com>
2021-01-09 22:24:30 -0300

    Day 2 solutions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,12 +5,14 @@ object Dependencies {
   object Versions {
     val cats = "2.3.1"
     val catsEffect = "2.3.1"
+    val catsParse = "0.2.0"
     val fs2 = "2.5.0"
   }
 
   object Libraries {
     val cats = "org.typelevel" %% "cats-core" % Versions.cats
     val catsEffect = "org.typelevel" %% "cats-effect" % Versions.catsEffect
+    val catsParse = "org.typelevel" %% "cats-parse" % Versions.catsParse
     val fs2Core = "co.fs2" %% "fs2-core" % Versions.fs2
     val fs2Io = "co.fs2" %% "fs2-io" % Versions.fs2
   }
--- src/main/scala/Utils/Fs2.scala
+++ src/main/scala/Utils/Fs2.scala
@@ -8,12 +8,11 @@ object Fs2 {
 
   implicit val ioContextShift: ContextShift[IO] = IO.contextShift(scala.concurrent.ExecutionContext.Implicits.global)
 
-  def readFileAsIntStream(filename: String): Stream[IO, Int] = Stream.resource(Blocker[IO]).flatMap { blocker =>
+  def readFile(filename: String): Stream[IO, String] = Stream.resource(Blocker[IO]).flatMap { blocker =>
     io.file
       .readAll[IO](Paths.get(filename), blocker, 4096)
       .through(text.utf8Decode)
       .through(text.lines)
-      .map(_.toInt)
   }
 
 }
